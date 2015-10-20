00359aa60da7b91d95343b530230e91a67e8b529
Cody Johnson <cajohnson7@bsu.edu>
2015-10-20 15:29:10 -0400

    FIrst commit. Contains start for select ingredients.

---
+++ app/src/androidTest/java/cajohnson7/cs222/bsu/edu/shoppinglist/ApplicationTest.java
@@ -0,0 +1,13 @@
+package cajohnson7.cs222.bsu.edu.shoppinglist;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
